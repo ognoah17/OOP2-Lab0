1ac7736826491c3841565ee404159ecfdea9b103
ognoah17 <noahjobse@gmail.com>
2025-01-16 22:03:35 -0700

    Commit

--- obj/Debug/net8.0/app.AssemblyInfo.cs
+++ obj/Debug/net8.0/app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fab1bd13237b57ed88e3f9f573b7fe73d69c0f29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3539210d5ddfbc4810cc52d8ef6536ae6d3cbfc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
